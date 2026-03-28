1c076264b1f0b2988a34154cf382f50f6b8d1c4b
chamidu gihan <w.g.c.g.ellepola@gmail.com>
2026-03-28 16:24:35 +0530

    small chages

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRouter from "./routes/auth.routes.js";
 import messageRouter from "./routes/message.routes.js";
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to get json data(from.body)
@@ -19,6 +22,12 @@ app.use(cookieParser()); //to get cookies
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*splat", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "bist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //   //http://localhost:5000/
